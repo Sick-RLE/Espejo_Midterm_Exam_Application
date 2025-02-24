eb9868fc53e9a0406be85c111017ce7903da6af2
RobinJamesL.Espejo <maanrigar22@gmail.com>
2025-02-24 20:58:50 +0800

    Added routes for movie's product listing and details

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
+Route::get('/movies', [ProductController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
